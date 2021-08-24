fb97b075cdc8c2a5159ceebbb904ddacd2b12e09
96031-75154 <89204724+96031-75154@users.noreply.github.com>
2021-08-24 17:31:46 +0530

    Day 1 Assignment Submitted

---
+++ day 1 assignment.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=8
+>>> y=5
+>>> x**y
+32768
+>>> a=4
+>>> b=7
+>>> a**b
+16384
+>>> m=11
+>>> n=13
+>>> m**n
+34522712143931
+>>> 
